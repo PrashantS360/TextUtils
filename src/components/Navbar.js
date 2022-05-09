2163dfd75fda92c1ff324d5ae592f7009df5c828
Prashant Singh <prashantsingh5aug@gmail.com>
2022-05-09 15:05:49 +0530

    Initial Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <Link className="navbar-brand" href="#">{props.title}</Link>
+                <span className="navbar-brand">{props.title}</span>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+                            <span className="nav-link active" aria-current="page" >Home</span>
                         </li>
                     </ul>
                     <div className="colPallete d-flex">
